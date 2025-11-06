256d043f61676d5c90a7df5424a8deb623285ff2
ledeus <alexey.mineev@unikie.com>
2025-11-06 23:31:49 +0200

    mt29 write: delay

--- drivers/mtd/nand/scai_mt29f/scai_mt29f.c
+++ drivers/mtd/nand/scai_mt29f/scai_mt29f.c
@@ -346,7 +346,7 @@ static int scai_nand_wait_idle(struct scai_nand_priv *priv)
 		if (status & STATUS1_IDLE) {
 			return 0; /* Success */
 		}
-		udelay(1);
+		mdelay(1);
 		retries--;
 	} while (retries > 0);
 
