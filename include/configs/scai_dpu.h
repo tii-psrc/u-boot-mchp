0697519d8e5b68401b4f4e4963a860fc93b73032
kwangsu-unikie <kwangsu.jung@unikie.com>
2026-03-27 10:04:25 +0000

    fix: default BOOTENV for DPU/NAVC

--- include/configs/scai_dpu.h
+++ include/configs/scai_dpu.h
@@ -57,6 +57,69 @@
 		"done; " \
 	"fi;\0 " \
 
+#define BOOTENV_DPU \
+	"bootargs_base=earlycon console=ttyS1,921600n8 " \
+		"uio_pdrv_genirq.of_id=generic-uio " \
+		"root=ubi0:rootfs rootfstype=ubifs rootwait rw\0" \
+	"set_bootargs=setenv bootargs ${bootargs_base} ubi.mtd=ubi_${active_slot}\0" \
+	"get_inactive=" \
+		"if test \"${active_slot}\" = \"a\"; then inactive_slot=b; " \
+		"else inactive_slot=a; fi\0" \
+	"boot_scripts=boot.scr\0" \
+	"boot_prefixes=/ /boot/\0" \
+	"boot_a_script=" \
+		"load ${devtype} ${devnum}:${distro_bootpart} ${scriptaddr} ${prefix}${script}; " \
+		"source ${scriptaddr}\0" \
+	"scan_dev_for_scripts=" \
+		"for script in ${boot_scripts}; do " \
+			"if test -e ${devtype} ${devnum}:${distro_bootpart} ${prefix}${script}; then " \
+				"echo Found U-Boot script ${prefix}${script}; " \
+				"run boot_a_script; " \
+				"echo SCRIPT FAILED: continuing...; " \
+			"fi; " \
+		"done\0" \
+	"scan_dev_for_boot=" \
+		"echo Scanning ${devtype} ${devnum}:${distro_bootpart}...; " \
+		"for prefix in ${boot_prefixes}; do run scan_dev_for_scripts; done;\0" \
+	"ubifs_boot=" \
+		"if ubi part ${bootubipart} ${bootubioff} && " \
+		   "ubifsmount ubi0:${bootubivol}; then " \
+			"devtype=ubi; devnum=ubi0; bootfstype=ubifs; " \
+			"distro_bootpart=${bootubivol}; " \
+			"run scan_dev_for_boot; " \
+			"ubifsumount; " \
+		"fi\0" \
+	"bootcmd_ubifs_slot=" \
+		"echo Booting slot ${slot}; " \
+		"bootubipart=ubi_${slot}; bootubivol=rootfs; bootubioff=; " \
+		"run ubifs_boot;\0" \
+	"default_active_slot=a\0" \
+	"ensure_active_slot=" \
+		"if env exists active_slot; then " \
+			"echo \"Using active_slot from HSS: ${active_slot}\"; " \
+		"else " \
+			"echo \"active_slot not set, using default\"; " \
+			"active_slot=${default_active_slot}; " \
+		"fi; " \
+		"if test \"${active_slot}\" != \"a\" && test \"${active_slot}\" != \"b\"; then " \
+			"echo \"active_slot invalid, using default\"; " \
+			"active_slot=${default_active_slot}; " \
+		"fi\0" \
+	"bootcmd_ubifs=" \
+		"run ensure_active_slot; " \
+		"run set_bootargs; " \
+		"echo === [BOOT] ACTIVE SLOT:${active_slot} ===; " \
+		"slot=${active_slot}; " \
+		"run bootcmd_ubifs_slot; " \
+		"echo === [BOOT] FAILOVER ===; " \
+		"run get_inactive; " \
+		"slot=${inactive_slot}; " \
+		"run bootcmd_ubifs_slot;\0" \
+	"boot_targets=ubifs\0" \
+	"distro_bootcmd=for target in ${boot_targets}; do run bootcmd_${target}; done\0" \
+	"bootcmd=run distro_bootcmd\0" \
+
+
 #if !defined(CONFIG_FIT_SIGNATURE)
 #include <config_distro_bootcmd.h>
 
@@ -67,8 +130,7 @@
 	"fdtoverlay_addr_r=0x8a080000\0" \
 	"ramdisk_addr_r=0x8aa00000\0" \
 	"scriptaddr=0x8e000000\0" \
-	BOOTENV_DESIGN_OVERLAYS \
-	BOOTENV \
+	BOOTENV_DPU \
 
 #endif
 #endif /* __CONFIG_H */
